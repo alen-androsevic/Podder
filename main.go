c24b7074d16954c1141da6eaa2cb229c4e350aa8
Alen Androsevic | UCare4 <alen@ucare4.nl>
2019-05-08 12:48:24 +0200

    init

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/SlyMarbo/rss"
+	"bufio"
+	"log"
+	"os"
+        "html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+type Podcast struct {
+	Title string
+	Link string
+}
+
+var pods []Podcast
+
+type ViewData struct{
+    Podcasts []Podcast
+}
+
+func main() {
+	feeds, err := os.Open("feeds")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer feeds.Close()
+	scanner := bufio.NewScanner(feeds)
+	for scanner.Scan() {
+		feed, err := rss.Fetch(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for index := 0; index < len(feed.Items); index++ {
+			url := feed.Items[index].Enclosures[0].URL
+			podcast := Podcast{
+				Title: feed.Items[index].Title,
+				Link: url,
+			}
+			pods = append(pods, podcast)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	tpl, err = template.ParseFiles("feeds.gohtml")
+	if err != nil {
+          log.Fatal(err)
+        }
+
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/style.css", serveResource)
+	http.HandleFunc("/favicon-32x32.png", serveResource)
+	http.HandleFunc("/favicon-16x16.png", serveResource)
+	http.HandleFunc("/site.webmanifest", serveResource)
+	http.ListenAndServe(":3000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	vd := ViewData{Podcasts: pods,}
+
+	err := tpl.Execute(w, vd)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func serveResource(w http.ResponseWriter, req *http.Request) {
+	path := "." + req.URL.Path
+	http.ServeFile(w, req, path)
+}
